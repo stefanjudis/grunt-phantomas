1ce99d074a0160bf52793ed9d19fd6ca0254bf8c
stefan judis <stefanjudis@gmail.com>
2014-04-09 21:54:25 +0200

    added jshint check to 'grunt test' command

--- Gruntfile.js
+++ Gruntfile.js
@@ -142,7 +142,7 @@ module.exports = function( grunt ) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask( 'test', [ 'clean', 'nodeunit' ] );
+  grunt.registerTask( 'test', [ 'clean', 'nodeunit', 'jshint' ] );
 
   // By default, lint and run all tests.
   grunt.registerTask( 'default', [ 'jshint', 'test' ] );
